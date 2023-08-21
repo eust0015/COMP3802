7636fd104552637e53176660ec29495be3960949
Bob <robsuniwork@gmail.com>
2023-08-21 14:44:02 +0930

    project initialisation

---
+++ Clipper1/Library/Bee/Android/Prj/IL2CPP/Gradle/launcher/build/generated/source/buildConfig/release/com/COMP3802/clipper1/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.COMP3802.clipper1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.COMP3802.clipper1";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
